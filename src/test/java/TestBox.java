a784008b6c575d55b740620963bb0cadb944acb6
utente <lorenzodiaz2002.l.d@gmail.com>
2024-02-26 17:25:10 +0100

    Class Box and relatives tests

--- src/test/java/TestBox.java
+++ src/test/java/TestBox.java
@@ -1,2 +1,17 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class TestBox {
+  @Test
+  void assertGetterTrue() {
+    Box box = new Box(false);
+    Assertions.assertTrue(box.isTaken());
+  }
+
+  @Test
+  void assertGetterFalse() {
+    Box box = new Box(false);
+    Assertions.assertFalse(box.isTaken());
+  }
+
 }
