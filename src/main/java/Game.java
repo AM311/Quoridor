85a8f1d61941650e8e737a4c7e01c3861aab758e
matfumis <matfumis@gmail.com>
2024-02-27 16:16:15 +0100

    Updated Game and created GameTest

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,6 +1,19 @@
 import java.util.List;
 
 public class Game {
-  private List<Player> players;
-  private GameBoard gameBoard;
+  private final List<Player> players;
+  private final GameBoard gameBoard;
+
+  public Game(List<Player> players, GameBoard gameBoard) {
+    this.players = players;
+    this.gameBoard = gameBoard;
+  }
+
+  public List<Player> getPlayers() {
+    return players;
+  }
+
+  public GameBoard getGameBoard() {
+    return gameBoard;
+  }
 }
