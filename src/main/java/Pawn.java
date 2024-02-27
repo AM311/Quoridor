034988d9595ff0cf03d7e39e7c9c97eecc33460e
matfumis <matfumis@gmail.com>
2024-02-27 15:20:37 +0100

    Completed Pawn

--- src/main/java/Pawn.java
+++ src/main/java/Pawn.java
@@ -1,3 +1,23 @@
+import java.awt.*;
+
 public class Pawn {
 	private Box position;
+	private final Color color;
+
+	public Pawn(Box position, Color color) {
+		this.position = position;
+		this.color = color;
+	}
+
+	public Box getPosition() {
+		return position;
+	}
+
+	public Color getColor() {
+		return color;
+	}
+
+	public void setPosition(Box position) {
+		this.position = position;
+	}
 }
