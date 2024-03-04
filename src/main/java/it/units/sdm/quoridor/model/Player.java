5c69b6f83cc89599450a83df73060d6b36a8ac02
DanieleMaijnelli <dani.maijnelli2002@gmail.com>
2024-03-04 14:34:01 +0100

    General Refactor, 2 spaces indentation. added new test class, added methods for verifying existence of a path.

--- src/main/java/it/units/sdm/quoridor/model/Player.java
+++ src/main/java/it/units/sdm/quoridor/model/Player.java
@@ -7,106 +7,106 @@ import static it.units.sdm.quoridor.model.GameBoard.LinkState.WALL;
 import static it.units.sdm.quoridor.utils.Direction.*;
 
 public class Player {
-	private final String name;
-	private final Pawn pawn;
-	private int numberOfWalls;
+  private final String name;
+  private final Pawn pawn;
+  private int numberOfWalls;
 
-	//todo add checks on walls number
-	//todo add wall number decrease
-	//todo add behavior in placeWall if checkWallPosition returns false
-	//todo check if a wall blocks completely the opponent
+  //todo add checks on walls number
+  //todo add wall number decrease
+  //todo add behavior in placeWall if checkWallPosition returns false
+  //todo check if a wall blocks completely the opponent
 
-	public Player(String name, int numberOfWalls, Pawn pawn) {
-		this.name = name;
-		this.numberOfWalls = numberOfWalls;
-		this.pawn = pawn;
-	}
+  public Player(String name, int numberOfWalls, Pawn pawn) {
+    this.name = name;
+    this.numberOfWalls = numberOfWalls;
+    this.pawn = pawn;
+  }
 
-	public void placeWall(GameBoard gameBoard, WallOrientation orientation, Tile startingTile) {
-		if (checkWallPosition(gameBoard, orientation, startingTile)) {
-			setWallLinks(gameBoard, orientation, startingTile);
-		}
-	}
+  public void placeWall(GameBoard gameBoard, WallOrientation orientation, Tile startingTile) {
+    if (checkWallPosition(gameBoard, orientation, startingTile)) {
+      setWallLinks(gameBoard, orientation, startingTile);
+    }
+  }
 
-	public boolean checkWallPosition(GameBoard gameBoard, WallOrientation orientation, Tile startingTile) {
-		return switch (orientation) {
-			case HORIZONTAL -> checkHorizontalWallPosition(gameBoard, startingTile);
-			case VERTICAL -> checkVerticalWallPosition(gameBoard, startingTile);
-		};
-	}
+  public boolean checkWallPosition(GameBoard gameBoard, WallOrientation orientation, Tile startingTile) {
+    return switch (orientation) {
+      case HORIZONTAL -> checkHorizontalWallPosition(gameBoard, startingTile);
+      case VERTICAL -> checkVerticalWallPosition(gameBoard, startingTile);
+    };
+  }
 
-	private void setWallLinks(GameBoard gameBoard, WallOrientation orientation, Tile startingTile) {
-		switch (orientation) {
-			case HORIZONTAL -> setWallLinksForHorizontalWall(gameBoard, startingTile);
-			case VERTICAL -> setWallLinkForVerticalWall(gameBoard, startingTile);
-		}
-	}
+  private void setWallLinks(GameBoard gameBoard, WallOrientation orientation, Tile startingTile) {
+    switch (orientation) {
+      case HORIZONTAL -> setWallLinksForHorizontalWall(gameBoard, startingTile);
+      case VERTICAL -> setWallLinkForVerticalWall(gameBoard, startingTile);
+    }
+  }
 
-	private boolean checkHorizontalWallPosition(GameBoard gameBoard, Tile startingTile) {
-		if (gameBoard.isInLastRow(startingTile) || gameBoard.isInLastColumn(startingTile)) {
-			return false;
-		}
-		//todo extract method "checkCrossingWalls"?
-		Tile tileBelowStartingTile = gameBoard.getLowerTile(startingTile);
-		Tile tileRightToStartingTile = gameBoard.getRightTile(startingTile);
+  private boolean checkHorizontalWallPosition(GameBoard gameBoard, Tile startingTile) {
+    if (gameBoard.isInLastRow(startingTile) || gameBoard.isInLastColumn(startingTile)) {
+      return false;
+    }
+    //todo extract method "checkCrossingWalls"?
+    Tile tileBelowStartingTile = gameBoard.getLowerTile(startingTile);
+    Tile tileRightToStartingTile = gameBoard.getRightTile(startingTile);
 
-		if (startingTile.getLink(RIGHT) == WALL && tileBelowStartingTile.getLink(RIGHT) == WALL) {
-			return false;
-		}
-		if (startingTile.getLink(DOWN) == WALL || tileRightToStartingTile.getLink(DOWN) == WALL) {
-			return false;
-		}
-		return true;
-	}
+    if (startingTile.getLink(RIGHT) == WALL && tileBelowStartingTile.getLink(RIGHT) == WALL) {
+      return false;
+    }
+    if (startingTile.getLink(DOWN) == WALL || tileRightToStartingTile.getLink(DOWN) == WALL) {
+      return false;
+    }
+    return true;
+  }
 
-	private boolean checkVerticalWallPosition(GameBoard gameBoard, Tile startingTile) {
-		if (gameBoard.isInFirstRow(startingTile) || gameBoard.isInFirstColumn(startingTile)) {
-			return false;
-		}
-		//todo extract method "checkCrossingWalls"?
-		Tile tileAboveStartingTile = gameBoard.getUpperTile(startingTile);
-		Tile tileLeftToStartingTile = gameBoard.getLeftTile(startingTile);
+  private boolean checkVerticalWallPosition(GameBoard gameBoard, Tile startingTile) {
+    if (gameBoard.isInFirstRow(startingTile) || gameBoard.isInFirstColumn(startingTile)) {
+      return false;
+    }
+    //todo extract method "checkCrossingWalls"?
+    Tile tileAboveStartingTile = gameBoard.getUpperTile(startingTile);
+    Tile tileLeftToStartingTile = gameBoard.getLeftTile(startingTile);
 
-		if (startingTile.getLink(UP) == WALL && tileLeftToStartingTile.getLink(UP) == WALL) {
-			return false;
-		}
-		if (startingTile.getLink(LEFT) == WALL || tileAboveStartingTile.getLink(LEFT) == WALL) {
-			return false;
-		}
-		return true;
-	}
+    if (startingTile.getLink(UP) == WALL && tileLeftToStartingTile.getLink(UP) == WALL) {
+      return false;
+    }
+    if (startingTile.getLink(LEFT) == WALL || tileAboveStartingTile.getLink(LEFT) == WALL) {
+      return false;
+    }
+    return true;
+  }
 
-	private void setWallLinksForHorizontalWall(GameBoard gameBoard, Tile startingTile) {
-		Tile tileBelowStartingTile = gameBoard.getLowerTile(startingTile);
-		Tile tileRightToStartingTile = gameBoard.getRightTile(startingTile);
-		Tile tileLowRightDiagToStartingTile = gameBoard.getLowerTile(gameBoard.getRightTile(startingTile));
+  private void setWallLinksForHorizontalWall(GameBoard gameBoard, Tile startingTile) {
+    Tile tileBelowStartingTile = gameBoard.getLowerTile(startingTile);
+    Tile tileRightToStartingTile = gameBoard.getRightTile(startingTile);
+    Tile tileLowRightDiagToStartingTile = gameBoard.getLowerTile(gameBoard.getRightTile(startingTile));
 
-		startingTile.setLink(DOWN, WALL);
-		tileRightToStartingTile.setLink(DOWN, WALL);
-		tileBelowStartingTile.setLink(UP, WALL);
-		tileLowRightDiagToStartingTile.setLink(UP, WALL);
-	}
+    startingTile.setLink(DOWN, WALL);
+    tileRightToStartingTile.setLink(DOWN, WALL);
+    tileBelowStartingTile.setLink(UP, WALL);
+    tileLowRightDiagToStartingTile.setLink(UP, WALL);
+  }
 
-	private void setWallLinkForVerticalWall(GameBoard gameBoard, Tile startingTile) {
-		Tile tileAboveStartingTile = gameBoard.getUpperTile(startingTile);
-		Tile tileLeftToStartingTile = gameBoard.getLeftTile(startingTile);
-		Tile tileUpLeftDiagToStartingTile = gameBoard.getUpperTile(gameBoard.getLeftTile(startingTile));
+  private void setWallLinkForVerticalWall(GameBoard gameBoard, Tile startingTile) {
+    Tile tileAboveStartingTile = gameBoard.getUpperTile(startingTile);
+    Tile tileLeftToStartingTile = gameBoard.getLeftTile(startingTile);
+    Tile tileUpLeftDiagToStartingTile = gameBoard.getUpperTile(gameBoard.getLeftTile(startingTile));
 
-		startingTile.setLink(LEFT, WALL);
-		tileAboveStartingTile.setLink(LEFT, WALL);
-		tileLeftToStartingTile.setLink(RIGHT, WALL);
-		tileUpLeftDiagToStartingTile.setLink(RIGHT, WALL);
-	}
+    startingTile.setLink(LEFT, WALL);
+    tileAboveStartingTile.setLink(LEFT, WALL);
+    tileLeftToStartingTile.setLink(RIGHT, WALL);
+    tileUpLeftDiagToStartingTile.setLink(RIGHT, WALL);
+  }
 
-	public String getName() {
-		return name;
-	}
+  public String getName() {
+    return name;
+  }
 
-	public int getNumberOfWalls() {
-		return numberOfWalls;
-	}
+  public int getNumberOfWalls() {
+    return numberOfWalls;
+  }
 
-	public Pawn getPawn() {
-		return pawn;
-	}
+  public Pawn getPawn() {
+    return pawn;
+  }
 }
--- src/main/java/it/units/sdm/quoridor/utils/WallOrientation.java
+++ src/main/java/it/units/sdm/quoridor/utils/WallOrientation.java
@@ -1,5 +1,5 @@
 package it.units.sdm.quoridor.utils;
 
 public enum WallOrientation {
-    HORIZONTAL, VERTICAL
+  HORIZONTAL, VERTICAL
 }
