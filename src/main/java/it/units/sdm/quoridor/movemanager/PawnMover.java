1ddcf57a42ab5004c7cbaef66a90327dfbbd0e0b
Alessio Manià <alessio.mania311@gmail.com>
2024-03-09 22:39:54 +0100

    FIX: removed duplicated method call

--- src/main/java/it/units/sdm/quoridor/movemanager/PawnMover.java
+++ src/main/java/it/units/sdm/quoridor/movemanager/PawnMover.java
@@ -5,12 +5,8 @@ import it.units.sdm.quoridor.model.GameBoard.Tile;
 import it.units.sdm.quoridor.model.Pawn;
 
 public class PawnMover implements Action<Tile> {
-	PawnMovementChecker pawnMovementChecker = new PawnMovementChecker();
-
 	@Override
 	public void execute(GameBoard gameBoard, Pawn pawn, Tile target) {
-		pawnMovementChecker.checkAction(gameBoard, pawn, target);
+		//todo IMPLEMENT METHOD!
 	}
-
-
 }
