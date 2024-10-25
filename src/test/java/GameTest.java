f8eac93a11f17aa457e34c923075f7b2f253ed28
Alessio Manià <alessio.mania311@gmail.com>
2024-10-25 10:48:12 +0200

    [skip ci] Minor corrections to test in GameTest

--- src/test/java/GameTest.java
+++ src/test/java/GameTest.java
@@ -6,6 +6,7 @@ import it.units.sdm.quoridor.model.*;
 import it.units.sdm.quoridor.model.builder.BuilderDirector;
 import it.units.sdm.quoridor.model.builder.StdQuoridorBuilder;
 import it.units.sdm.quoridor.utils.Position;
+import it.units.sdm.quoridor.utils.WallOrientation;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -95,8 +96,8 @@ public class GameTest {
 
 
   @ParameterizedTest
-  @CsvSource({"5, 3, 4, 3, 4, 3", "3, 6 , 2, 6, 2, 6", "8, 0, 7, 0, 7, 0"})
-  void jumpingOverHorizontalWallNotAllowed(int startingRow, int startingColumn, int targetRow, int targetColumn, int wallRow, int wallColumn) throws InvalidParameterException, BuilderException, InvalidActionException {
+  @CsvSource({"5, 3, 4, 3, 4, 3, HORIZONTAL", "3, 6 , 2, 6, 2, 6, HORIZONTAL", "1, 7, 1, 6, 1, 7, VERTICAL", "3, 3, 3, 2, 4, 3, VERTICAL"})
+  void jumpingOverWallNotAllowed(int startingRow, int startingColumn, int targetRow, int targetColumn, int wallRow, int wallColumn, WallOrientation wallOrientation) throws InvalidParameterException, BuilderException, InvalidActionException {
     AbstractGame game = buildGame();
 
     Position startingPosition = new Position(startingRow, startingColumn);
@@ -105,32 +106,14 @@ public class GameTest {
 
     movePawn(game, startingPosition);
 
-    game.placeWall(wallPosition, HORIZONTAL);
+    game.placeWall(wallPosition, wallOrientation);
 
     Assertions.assertThrows(InvalidActionException.class, () -> game.movePlayingPawn(targetPosition));
   }
 
-
   @ParameterizedTest
-  @CsvSource({"1, 7, 1, 6, 1, 7", "3, 3, 3, 2, 4, 3", "8, 1, 8, 0, 8, 1", "5, 7, 5, 8, 6 , 8"})
-  void jumpingOverVerticalWallNotAllowed(int startingRow, int startingColumn, int targetRow, int targetColumn, int wallRow, int wallColumn) throws InvalidParameterException, BuilderException, InvalidActionException {
-    AbstractGame game = buildGame();
-
-    Position startingPosition = new Position(startingRow, startingColumn);
-    Position wallPosition = new Position(wallRow, wallColumn);
-    Position targetPosition = new Position(targetRow, targetColumn);
-
-    movePawn(game, startingPosition);
-
-    game.placeWall(wallPosition, VERTICAL);
-
-    Assertions.assertThrows(InvalidActionException.class, () -> game.movePlayingPawn(targetPosition));
-  }
-
-
-  @ParameterizedTest
-  @CsvSource({"3, 1, 3, 2, 3, 3, 4, 3", "6, 6, 6, 5, 6, 4, 6, 5", "8, 6, 8, 7, 8, 8, 8, 8"})
-  void jumpingOverPawnHavingVerticalWallBehindNotAllowed(int startingRow, int startingColumn, int opponentRow, int opponentColumn, int targetRow, int targetColumn, int wallRow, int wallColumn) throws InvalidParameterException, BuilderException, InvalidActionException {
+  @CsvSource({"7, 1, 6, 1, 5, 1, 5, 1, HORIZONTAL", "2, 1, 1, 1, 0, 1, 0, 0, HORIZONTAL", "6, 6, 6, 5, 6, 4, 6, 5, VERTICAL", "8, 6, 8, 7, 8, 8, 8, 8, VERTICAL"})
+  void jumpingOverPawnHavingWallBehindNotAllowed(int startingRow, int startingColumn, int opponentRow, int opponentColumn, int targetRow, int targetColumn, int wallRow, int wallColumn, WallOrientation wallOrientation) throws InvalidParameterException, BuilderException, InvalidActionException {
     AbstractGame game = buildGame();
 
     Position startingPosition = new Position(startingRow, startingColumn);
@@ -144,57 +127,10 @@ public class GameTest {
     movePawn(game, opponentPosition);
     game.changeRound();
 
-    game.placeWall(wallPosition, VERTICAL);
+    game.placeWall(wallPosition, wallOrientation);
 
     Assertions.assertThrows(InvalidActionException.class, () -> game.movePlayingPawn(targetPosition));
   }
-
-
-  @ParameterizedTest
-  @CsvSource({"7, 1, 6, 1, 5, 1, 5, 1", "2, 1, 1, 1, 0, 1, 0, 0", "2, 6, 3, 6, 4, 6, 3, 5"})
-  void jumpingOverPawnHavingHorizontalWallBehindNotAllowed(int startingRow, int startingColumn, int opponentRow, int opponentColumn, int targetRow, int targetColumn, int wallRow, int wallColumn) throws InvalidParameterException, BuilderException, InvalidActionException {
-    AbstractGame game = buildGame();
-
-    Position startingPosition = new Position(startingRow, startingColumn);
-    Position opponentPosition = new Position(opponentRow, opponentColumn);
-    Position wallPosition = new Position(wallRow, wallColumn);
-    Position targetPosition = new Position(targetRow, targetColumn);
-
-    movePawn(game, startingPosition);
-
-    game.changeRound();
-    movePawn(game, opponentPosition);
-    game.changeRound();
-
-    game.placeWall(wallPosition, HORIZONTAL);
-
-    Assertions.assertThrows(InvalidActionException.class, () -> game.movePlayingPawn(targetPosition));
-  }
-
-
-  @ParameterizedTest
-  @CsvSource({"7, 7, 7, 6, 6, 6, 8, 6, 6, 5", "3, 2, 2, 2, 2, 1, 2, 2, 1, 2", "2, 7, 3, 7, 3, 8, 4, 8, 3, 6"})
-  void goingDiagonalIfThereAreWallsPlacedAsTNotAllowed(int startingRow, int startingColumn, int opponentRow, int opponentColumn, int targetRow, int targetColumn, int verticalWallRow, int verticalWallColumn, int horizontalWallRow, int horizontalWallColumn) throws InvalidParameterException, BuilderException, InvalidActionException {
-    AbstractGame game = buildGame();
-
-    Position startingPosition = new Position(startingRow, startingColumn);
-    Position opponentPosition = new Position(opponentRow, opponentColumn);
-    Position horizontalwallPosition = new Position(horizontalWallRow, horizontalWallColumn);
-    Position verticalwallPosition = new Position(verticalWallRow, verticalWallColumn);
-    Position targetPosition = new Position(targetRow, targetColumn);
-
-    movePawn(game, startingPosition);
-
-    game.changeRound();
-    movePawn(game, opponentPosition);
-    game.changeRound();
-
-    game.placeWall(verticalwallPosition, VERTICAL);
-    game.placeWall(horizontalwallPosition, HORIZONTAL);
-
-    Assertions.assertThrows(InvalidActionException.class, () -> game.movePlayingPawn(targetPosition));
-  }
-
 
   @ParameterizedTest
   @CsvSource({"4, 3, 3, 3, 2, 3, 1, 3", "4, 3, 4, 2, 4, 1, 4, 0", "4, 5, 4, 6, 4, 7, 4, 8", "4, 5, 5, 5, 6, 5, 7, 5"})
