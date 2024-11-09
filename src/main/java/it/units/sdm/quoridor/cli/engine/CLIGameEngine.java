05ae82cdb97c5b08326be97e76dcad06af2f7908
Matteo Fumis <matfumis@gmail.com>
2024-11-10 00:43:11 +0100

    feat(CLIGameEngine | CLIGameEngineTest): added methods implementation and relative tests

--- src/main/java/it/units/sdm/quoridor/cli/engine/CLIGameEngine.java
+++ src/main/java/it/units/sdm/quoridor/cli/engine/CLIGameEngine.java
@@ -7,12 +7,13 @@ import it.units.sdm.quoridor.model.AbstractGame;
 import it.units.sdm.quoridor.model.builder.BuilderDirector;
 import it.units.sdm.quoridor.model.builder.StdQuoridorBuilder;
 
-import java.util.Scanner;
 
-public class CLIGameEngine implements QuoridorGameEngine{
+public class CLIGameEngine implements QuoridorGameEngine {
 
   private final InputProvider inputProvider;
   private final QuoridorParser parser;
+
+  private boolean testingMode = false;
 
   public CLIGameEngine(InputProvider inputProvider, QuoridorParser parser) {
     this.inputProvider = inputProvider;
@@ -38,11 +39,53 @@ public class CLIGameEngine implements QuoridorGameEngine{
 
   @Override
   public void startGame(AbstractGame game) {
+    while(true){
+      executeRound(game);
 
+      if (testingMode) {
+        break;
+      }
+    }
   }
 
   @Override
   public void endGame() {
+    System.out.println("Game over!");
+  }
 
+  private void executeRound(AbstractGame game) {
+    printWallsConvention();
+
+    String command = inputProvider.nextLine();
+    performCommand(command);
+
+    if(game.isGameFinished()) {
+      System.out.println(game.getPlayingPawn() + " won!");
+      endGame();
+    }
+    game.changeRound();
+  }
+
+  private void performCommand(String command) {
+    System.out.println("Command " + command + " executed");
+  }
+
+  private void printWallsConvention() {
+    final String RED_SQUARE = "\u001B[31m■\u001B[0m";
+
+    System.out.println("How walls are placed:");
+
+    System.out.println("                          \n");
+    System.out.println("     +     +     +        \n");
+    System.out.println("     |                    \n");
+    System.out.println("  v  +     +     +        \n");
+    System.out.println("     |  " + RED_SQUARE + "\n");
+    System.out.println("     + ─── + ─── +        \n");
+    System.out.println("           h              \n");
+    System.out.println("                          \n");
+  }
+
+  public void enableTestingMode() {
+    this.testingMode = true;
   }
 }
--- src/test/java/CLIGameEngineTest.java
+++ src/test/java/CLIGameEngineTest.java
@@ -3,14 +3,25 @@ import it.units.sdm.quoridor.cli.engine.CLIInputProvider;
 import it.units.sdm.quoridor.cli.engine.InputProvider;
 import it.units.sdm.quoridor.cli.engine.QuoridorGameEngine;
 import it.units.sdm.quoridor.cli.parser.QuoridorParser;
+import it.units.sdm.quoridor.exceptions.InvalidParameterException;
+import it.units.sdm.quoridor.exceptions.ParserException;
 import it.units.sdm.quoridor.model.AbstractGame;
+import it.units.sdm.quoridor.model.AbstractTile;
+import it.units.sdm.quoridor.model.Tile;
+import it.units.sdm.quoridor.movemanagement.actionmanagers.ActionManager;
+import it.units.sdm.quoridor.movemanagement.actions.PawnMover;
+import it.units.sdm.quoridor.utils.Position;
+import it.units.sdm.quoridor.utils.WallOrientation;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import java.io.ByteArrayInputStream;
-import java.util.Scanner;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.Optional;
 
 public class CLIGameEngineTest {
 
@@ -68,6 +79,75 @@ public class CLIGameEngineTest {
 
     Assertions.assertNotNull(game);
   }
+
+  @Test
+  void inputCommandIsPerformed() {
+    String simulatedUserInput = "2\nmoveFirstIn1,4\n";
+    System.setIn(new ByteArrayInputStream(simulatedUserInput.getBytes()));
+
+
+    ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
+    System.setOut(new PrintStream(outputStreamCaptor));
+
+    InputProvider inputProvider = new CLIInputProvider();
+    CLIGameEngine cliGameEngine = new CLIGameEngine(inputProvider, parser);
+
+    cliGameEngine.enableTestingMode();
+    AbstractGame game = cliGameEngine.createGame();
+
+    cliGameEngine.startGame(game);
+
+    String output = outputStreamCaptor.toString();
+    Assertions.assertTrue(output.contains("Command moveFirstIn1,4 executed"));
+  }
+
+
+  @Test
+  void roundIsProperlyChanged() {
+    String simulatedUserInput = "2\nmoveFirstIn1,4\n";
+    System.setIn(new ByteArrayInputStream(simulatedUserInput.getBytes()));
+
+    InputProvider inputProvider = new CLIInputProvider();
+    CLIGameEngine cliGameEngine = new CLIGameEngine(inputProvider, parser);
+
+    cliGameEngine.enableTestingMode();
+    AbstractGame game = cliGameEngine.createGame();
+
+    cliGameEngine.startGame(game);
+
+    Assertions.assertEquals(game.getPlayingPawn(), game.getPawns().get(1));
+
+  }
+
+  @Test
+  void winningPawnProperlyRecognized() throws InvalidParameterException {
+    String simulatedUserInput = "2\nmakeFirstPawnWin\n";
+    System.setIn(new ByteArrayInputStream(simulatedUserInput.getBytes()));
+
+
+    ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
+    System.setOut(new PrintStream(outputStreamCaptor));
+
+    InputProvider inputProvider = new CLIInputProvider();
+    CLIGameEngine cliGameEngine = new CLIGameEngine(inputProvider, parser);
+
+    cliGameEngine.enableTestingMode();
+    AbstractGame game = cliGameEngine.createGame();
+
+    PawnMover pawnMover = new PawnMover();
+    AbstractTile target = game.getGameBoard().getTile(new Position(1,5));
+    pawnMover.execute(game, target);
+
+    while(game.getPawns().getFirst().getCurrentTile().getRow() != 8){
+      pawnMover.execute(game, game.getGameBoard().getTile(new Position(game.getPawns().getFirst().getCurrentTile().getRow() + 1,5)));
+    }
+
+    cliGameEngine.startGame(game);
+
+    String output = outputStreamCaptor.toString();
+    Assertions.assertTrue(output.contains(game.getPawns().getFirst() + " won!"));
+  }
+
 
 
 
