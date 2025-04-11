3182ede19f800d70143c8c5f052c7ebcff25a0ea
CeciliaComar <116801932+CeciliaComar@users.noreply.github.com>
2025-04-11 14:31:20 +0200

    perf(GameStringBuilder): implemented string builder

--- src/main/java/it/units/sdm/quoridor/cli/GameStringBuilder.java
+++ src/main/java/it/units/sdm/quoridor/cli/GameStringBuilder.java
@@ -8,24 +8,26 @@ import java.util.List;
 public class GameStringBuilder {
 
   public static String createGameString(AbstractGame game) {
-    String gameString = "";
 
-    gameString += game.getGameBoard().toString();
-    gameString += "\n\n";
+    StringBuilder gameString = new StringBuilder(game.getGameBoard().toString());
+    gameString.append("\n\n").append(game.getPlayingPawn()).append("'s turn").append("\n\n");
+    appendRemainingWallsInfoString(game, gameString);
 
-    gameString += game.getPlayingPawn() + "'s turn\n\n";
+    return gameString.toString();
+  }
 
-    gameString += "Remaining Walls:\n\n";
+  private static void appendRemainingWallsInfoString(AbstractGame game, StringBuilder gameString) {
+    gameString.append("Remaining Walls:").append("\n\n");
 
     List<String> pawnInfoList = new ArrayList<>();
+
     for (AbstractPawn pawn : game.getPawns()) {
       int remainingWalls = pawn.getNumberOfWalls();
       pawnInfoList.add(pawn + " (" + remainingWalls + ")");
+
     }
+    gameString.append(String.join("  -  ", pawnInfoList));
 
-    gameString += String.join("  -  ", pawnInfoList);
-
-    return gameString;
   }
 
 }
