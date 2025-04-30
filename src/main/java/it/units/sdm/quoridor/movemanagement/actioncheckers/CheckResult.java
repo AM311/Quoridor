c95281d636ab07433511a3cd3f398c755a496a8c
Cecilia <cecilia.comar01@gmail.com>
2025-04-30 16:59:25 +0200

    feat(CheckResult): new enum for mapping results of Checkers

---
+++ src/main/java/it/units/sdm/quoridor/movemanagement/actioncheckers/CheckResult.java
@@ -0,0 +1,5 @@
+package it.units.sdm.quoridor.movemanagement.actioncheckers;
+
+public enum CheckResult {
+  OKAY, END_OF_AVAIABLE_WALLS, INVALID_WALL_POSITION, BLOCKING_WALL, INVALID_MOVEMENT, OCCUPIED_TILE
+}
